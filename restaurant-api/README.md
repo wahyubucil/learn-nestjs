baa63aff5aa90e1f271f906ecd4e58a6f26c7a2a
Wahyu Bucil <wahyubucil@gmail.com>
2020-01-23 23:08:18 +0800

    New tutorial from auth0

---
+++ restaurant-api/README.md
@@ -0,0 +1,3 @@
+## restaurant-api
+
+Learn from : https://auth0.com/blog/developing-a-secure-api-with-nestjs-models-data-service/
---
+++ restaurant-api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+bootstrap();
